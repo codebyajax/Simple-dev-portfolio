e612019a8dd265090f13a2a5f0095ddd1c9fb0da
ajaxuzi <Jackthe1first@gmail.com>
2026-04-13 12:04:18 +0100

    update layout and config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,44 +1,34 @@
+import type { Metadata } from "next";
 import { instagramSans } from "@/font";
 import "./globals.css";
 
+export const metadata: Metadata = {
+  title: "Jacques Azianou | Designer Engineer",
+  description:
+    "Helping brands thrive in the digital world. Located in Nigeria. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Jacques",
+
+  openGraph: {
+    title: "Jacques Azianou • Intuitive Designer & Developer",
+    description:
+      "Helping brands thrive in the digital world. Located in Nigeria. Delivering tailor-made digital designs and building interactive websites from scratch.",
+    siteName: "Jacques Azianou",
+    locale: "en",
+    type: "website",
+  },
+
+  metadataBase: new URL("https://yourdomain.com"), // change this later
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=5"
-        />
-        <title>Jacques Azianou | Designer Engineer</title>
-        <meta
-          name="description"
-          content="Helping brands thrive in the digital world. Located in The Nigeria. Delivering tailor-made digital designs and building interactive websites from scratch. © Code by Jacques"
-        />
-        <meta
-          property="og:title"
-          content="Jacques Azianou • Intuitive Designer &amp; 
-      Developer"
-        />
-        <meta
-          property="og:description"
-          content="Helping brands thrive in the digital world. Located in Nigeria. 
-      Delivering tailor-made digital designs and building interactive websites from scratch. © 
-      Code by Jacques"
-        />
-        <meta property="og:site_name" content="Jacques Azianou" />
-        <meta property="og:locale" content="en" />
-        <meta property="og:type" content="website" />
-        <meta name="google" content="notranslate" />
-        <meta property="og:locale" content="en" />
-      </head>
       <body className={`${instagramSans.variable} antialiased`}>
-          {children}
+        {children}
       </body>
     </html>
   );
-};
-
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   productionBrowserSourceMaps: true,
   reactStrictMode: true,
-
+  allowedDevOrigins: ["192.168.88.52"],
 };
+
 export default nextConfig;
